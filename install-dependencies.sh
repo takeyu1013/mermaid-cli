a95c74397cf9366ec69cca4c0c8ba0a0ee487b83
takeyu1013 <yuto1013@gmail.com>
2023-05-19 21:56:51 +0900

    ➕

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,4 +5,5 @@
 apk add chromium font-noto-cjk font-noto-emoji \
     terminus-font ttf-dejavu ttf-freefont ttf-font-awesome \
     ttf-inconsolata ttf-linux-libertine \
+    font-noto font-noto-thai font-noto-tibetan font-ipa font-sony-misc font-daewoo-misc \
     && fc-cache -f
